d2f5f07c29dd3ca202f02931c19ffb995bce80e4
Martyn Ranyard <mra@hellofresh.com>
2017-10-11 15:34:12 +0200

    Get some error reporting in there

--- main.go
+++ main.go
@@ -15,7 +15,29 @@ var (
 	app = kingpin.New("klepto", "Steals data from production to put on staging")
 
 	steal = app.Command("steal", "Steal a live database")
+	stealingDSN = steal.Flag("inputdsn", "DSN for the input database").Default("root:root@localhost/example").String()
+	swagDSN = steal.Flag("outputdsn", "DSN for the output database (or just 'STDOUT' for a dump)").Default("root:root@localhost/example").String()
 )
+
+func ensureConnectionIsGood(db *sql.DB) error {
+	tables := make([]string, 0)
+	var rows *sql.Rows
+	var err = error(nil)
+	if rows, err = db.Query("SHOW FULL TABLES"); err != nil {
+		return err
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var tableName, tableType string
+		if err := rows.Scan(&tableName, &tableType); err != nil {
+			return err
+		}
+		if tableType == "BASE TABLE" {
+			tables = append(tables, tableName)
+		}
+	}
+	return nil
+}
 
 func main() {
 	command, err := app.Parse(os.Args[1:])
@@ -25,15 +47,24 @@ func main() {
 		os.Exit(2)
 	}
 
-	dsn := "root:root@localhost/something" // TODO: take this from CLI
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", *stealingDSN)
 	logger := log.New(os.Stdout, "klepto: ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds)
+	if err != nil {
+		log.Fatalf("MySQL connection failed: %s \n", err)
+	}
+
+	err = ensureConnectionIsGood(db)
+	if err != nil {
+		log.Fatalf("Error in MySQL connection: %s \n", err)
+	}
 
 	switch command {
 	case steal.FullCommand():
 		d := dumper.NewMySQLDumper(db, logger)
 		// TODO: Define masks from config file
-		d.Dump(os.Stdout) // TODO: Define out as another mysql db (config from CLI)
+		if *swagDSN == "STDOUT" {
+			d.Dump(os.Stdout) // TODO: Define out as another mysql db (config from CLI)
+		}
 		break
 	}
 }

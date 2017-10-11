242ec3dc5e98ba12659aee2e3bf2a9a6e09720fd
Kieran <me@kieranajp.co.uk>
2017-10-11 13:51:17 +0200

    Try using mysqlsuperdumper

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"os"
 
 	"github.com/alecthomas/colour"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/hgfischer/mysqlsuperdump/dumper"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -21,8 +25,15 @@ func main() {
 		os.Exit(2)
 	}
 
+	dsn := "root:root@localhost/something" // TODO: take this from CLI
+	db, err := sql.Open("mysql", dsn)
+	logger := log.New(os.Stdout, "klepto: ", log.LstdFlags|log.Lshortfile|log.Lmicroseconds)
+
 	switch command {
 	case steal.FullCommand():
+		d := dumper.NewMySQLDumper(db, logger)
+		// TODO: Define masks from config file
+		d.Dump(os.Stdout) // TODO: Define out as another mysql db (config from CLI)
 		break
 	}
 }

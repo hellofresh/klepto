a133fcab2d7e01475dda73c1c4ef2635f28e91b3
Kieran <me@kieranajp.co.uk>
2017-10-24 19:45:37 +0200

    Added basic test for typescanner

--- database/mysql_anonymiser.go
+++ database/mysql_anonymiser.go
@@ -3,8 +3,8 @@ package database
 import (
 	"database/sql"
 	"fmt"
-	"time"
 
+	"github.com/hellofresh/klepto/utils"
 	"github.com/malisit/kolpa"
 	"github.com/spf13/viper"
 )
@@ -29,7 +29,7 @@ func (a *MySQLAnonymiser) DumpTable(table string, rowChan chan<- []*Cell, endCha
 	for rows.Next() {
 		row := make([]interface{}, len(columns))
 		for idx := range columns {
-			row[idx] = new(TypeScanner)
+			row[idx] = new(utils.TypeScanner)
 		}
 
 		err := rows.Scan(row...)
@@ -43,12 +43,12 @@ func (a *MySQLAnonymiser) DumpTable(table string, rowChan chan<- []*Cell, endCha
 			var cell *Cell
 			replacement := a.shouldAnonymise(table, column)
 
-			scanner := row[idx].(*TypeScanner)
+			scanner := row[idx].(*utils.TypeScanner)
 
-			if replacement != "" && scanner.detected != "null" {
-				cell = &Cell{Column: column, Type: scanner.detected, Value: k.GenericGenerator(replacement)}
+			if replacement != "" && scanner.Detected != "null" {
+				cell = &Cell{Column: column, Type: scanner.Detected, Value: k.GenericGenerator(replacement)}
 			} else {
-				cell = &Cell{Column: column, Type: scanner.detected, Value: scanner.value}
+				cell = &Cell{Column: column, Type: scanner.Detected, Value: scanner.Value}
 			}
 
 			cells = append(cells, cell)
@@ -63,63 +63,4 @@ func (a *MySQLAnonymiser) DumpTable(table string, rowChan chan<- []*Cell, endCha
 
 func (a *MySQLAnonymiser) shouldAnonymise(table, column string) string {
 	return viper.GetString(fmt.Sprintf("anonymise.%s.%s", table, column))
-}
-
-// TypeScanner tries to determine the type of a provided value
-type TypeScanner struct {
-	valid    bool
-	value    interface{}
-	detected string
-}
-
-func (scanner *TypeScanner) getBytes(src interface{}) []byte {
-	if a, ok := src.([]uint8); ok {
-		return a
-	}
-	return nil
-}
-
-// Scan accepts a value and attempts to determine its type
-func (scanner *TypeScanner) Scan(src interface{}) error {
-	switch src.(type) {
-	case int64:
-		if value, ok := src.(int64); ok {
-			scanner.value = value
-			scanner.valid = true
-			scanner.detected = "int"
-		}
-	case float64:
-		if value, ok := src.(float64); ok {
-			scanner.value = value
-			scanner.valid = true
-			scanner.detected = "float"
-		}
-	case bool:
-		if value, ok := src.(bool); ok {
-			scanner.value = value
-			scanner.valid = true
-			scanner.detected = "bool"
-		}
-	case string:
-		value := scanner.getBytes(src)
-		scanner.value = string(value)
-		scanner.valid = true
-		scanner.detected = "string"
-	case []byte:
-		value := scanner.getBytes(src)
-		scanner.value = string(value)
-		scanner.valid = true
-		scanner.detected = "string"
-	case time.Time:
-		if value, ok := src.(time.Time); ok {
-			scanner.value = value
-			scanner.valid = true
-			scanner.detected = "time"
-		}
-	case nil:
-		scanner.value = "NULL"
-		scanner.valid = true
-		scanner.detected = "null"
-	}
-	return nil
 }

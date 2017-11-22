27d2f068ee3e17b3ec993f07f28729fbfcda7cfc
Jojoo Imbeah <jojoo.imbeah@gmail.com>
2017-11-22 11:40:10 +0100

    Implement code style improvements from review

--- cmd/steal.go
+++ cmd/steal.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-
 	"regexp"
 
 	"github.com/fatih/color"
@@ -57,32 +56,46 @@ func RunSteal(cmd *cobra.Command, args []string) {
 
 // findForeignKeys connects to information_schema and retrieves all foreign keys.
 func findForeignKeys(dsn string) (err error) {
-	if pRecordType != "" {
-		// append 'information_schema' to trimmed dsn.
-		iSchemaDsn := fmt.Sprintf("%s%s", trimDb(fromDSN), "information_schema")
-		iSchemaConn, err := database.Connect(iSchemaDsn)
-		if err != nil {
-			color.Red("Error connecting to information_schema: %s", err.Error())
-		}
-		defer iSchemaConn.Close()
-
-		inputConn, err := database.Connect(fromDSN)
-		if err != nil {
-			color.Red("Error connecting to input database: %s", err.Error())
-		}
-
-		iStore := database.NewiSchemaStorage(iSchemaConn, inputConn)
-		res, err := iStore.Relationships()
-		fmt.Printf("%+v \n", res)
-		if err != nil {
-			color.Red("Error retrieving relationships from information_schema: %s", err.Error())
-		}
+	if pRecordType == "" {
+		return
 	}
+
+	// append 'information_schema' to trimmed dsn.
+	r, err := trimDBname(fromDSN)
+	if err != nil {
+		return err
+	}
+	iSchemaDsn := fmt.Sprintf("%s%s", r, "information_schema")
+	iSchemaConn, err := database.Connect(iSchemaDsn)
+	if err != nil {
+		color.Red("Error connecting to information_schema: %s", err.Error())
+		return err
+	}
+	defer iSchemaConn.Close()
+
+	inputConn, err := database.Connect(fromDSN)
+	if err != nil {
+		color.Red("Error connecting to input database: %s", err.Error())
+		return err
+	}
+
+	iStore := database.NewiSchemaStorage(iSchemaConn, inputConn)
+	res, err := iStore.Relationships()
+	fmt.Printf("%+v \n", res)
+	if err != nil {
+		color.Red("Error retrieving relationships from information_schema: %s", err.Error())
+		return err
+	}
+
 	return
 }
 
-// trimDb strips db name from the dsn
-func trimDb(dsn string) string {
-	r, _ := regexp.Compile(".*/")
-	return r.FindString(dsn)
+// trim strips db name from the dsn
+func trimDBname(dsn string) (string, error) {
+	r, err := regexp.Compile(".*/")
+	if err != nil {
+		return "", err
+	}
+	return r.FindString(dsn), nil
+
 }

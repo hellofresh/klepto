26a1fc9a135523023e180cc4d4a9128e71e7f94f
Italo Lelis de Vietro <italolelis@gmail.com>
2017-12-29 15:34:28 +0100

    Using a query builder to dump values

--- pkg/dumper/query/dumper.go
+++ pkg/dumper/query/dumper.go
@@ -2,73 +2,66 @@ package query
 
 import (
 	"fmt"
+	"io"
 	"strconv"
-	"strings"
 	"time"
 
-	"io"
-
+	sq "github.com/Masterminds/squirrel"
 	"github.com/hellofresh/klepto/pkg/config"
 	"github.com/hellofresh/klepto/pkg/database"
 	"github.com/hellofresh/klepto/pkg/dumper"
 	"github.com/hellofresh/klepto/pkg/reader"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
 // textDumper dumps a database's structure to a stream
 type textDumper struct {
-	reader       reader.Reader
-	output       io.Writer
-	configTables config.Tables
+	reader reader.Reader
+	output io.Writer
 }
 
 // NewDumper is the constructor for MySQLDumper
-func NewDumper(output io.Writer, rdr reader.Reader, configTables config.Tables) dumper.Dumper {
+func NewDumper(output io.Writer, rdr reader.Reader) dumper.Dumper {
 	return &textDumper{
-		reader:       rdr,
-		output:       output,
-		configTables: configTables,
+		reader: rdr,
+		output: output,
 	}
 }
 
-func (d *textDumper) Dump(done chan<- struct{}) error {
+func (d *textDumper) Dump(done chan<- struct{}, configTables config.Tables) error {
 	tables, err := d.reader.GetTables()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "could not get tables")
 	}
 
 	structure, err := d.reader.GetStructure()
 	if err != nil {
-		return err
+		return errors.Wrap(err, "could not get database structure")
 	}
 	io.WriteString(d.output, structure)
 
 	for _, tbl := range tables {
-		table, err := d.configTables.FindByName(tbl)
+		var opts reader.ReadTableOpt
+
+		table, err := configTables.FindByName(tbl)
 		if err != nil {
 			log.WithError(err).WithField("table", tbl).Debug("no configuration found for table")
 		}
 
-		columns, err := d.reader.GetColumns(tbl)
-		if err != nil {
-			return err
-		}
-
-		insert := fmt.Sprintf("\nINSERT INTO `%s` (%s) VALUES ", tbl, strings.Join(columns, ", "))
 		// Create read/write chanel
 		rowChan := make(chan database.Row)
 
 		if table != nil {
-			opts := reader.ReadTableOpt{
+			opts = reader.ReadTableOpt{
 				Limit:         table.Filter.Limit,
 				Relationships: d.relationshipConfigToOptions(table.Relationships),
 			}
-			go d.reader.ReadTable(tbl, rowChan, opts)
-		} else {
-			go d.reader.ReadTable(tbl, rowChan, reader.ReadTableOpt{})
 		}
 
-		go func() {
+		go d.reader.ReadTable(tbl, rowChan, opts)
+
+		go func(tableName string) {
 			for {
 				row, more := <-rowChan
 				if !more {
@@ -76,66 +69,71 @@ func (d *textDumper) Dump(done chan<- struct{}) error {
 					return
 				}
 
-				io.WriteString(d.output, insert)
-				io.WriteString(d.output, "(")
-				for i, column := range columns {
-					data := row[column]
-
-					if i > 0 {
-						io.WriteString(d.output, ",")
-					}
-
-					io.WriteString(d.output, d.toSQLStringValue(data.Value))
-				}
-				io.WriteString(d.output, ")")
-				io.WriteString(d.output, ";")
+				insert := sq.Insert(tableName).SetMap(d.toSQLColumnMap(row))
+				io.WriteString(d.output, sq.DebugSqlizer(insert))
+				io.WriteString(d.output, "\n")
 			}
-		}()
+		}(tbl)
 	}
 
 	return nil
 }
 
+func (d *textDumper) toSQLColumnMap(row database.Row) map[string]interface{} {
+	sqlColumnMap := make(map[string]interface{})
+
+	for column, value := range row {
+		strValue, err := d.toSQLStringValue(value)
+		if err != nil {
+			log.WithError(err).Debug("could not convert value to string")
+		}
+
+		sqlColumnMap[column] = fmt.Sprintf("%v", strValue)
+	}
+
+	return sqlColumnMap
+}
+
 // ResolveType accepts a value and attempts to determine its type
-func (d *textDumper) toSQLStringValue(src interface{}) string {
+func (d *textDumper) toSQLStringValue(src interface{}) (string, error) {
 	switch src.(type) {
 	case int64:
 		if value, ok := src.(int64); ok {
-			return strconv.FormatInt(value, 10)
+			return strconv.FormatInt(value, 10), nil
 		}
 	case float64:
 		if value, ok := src.(float64); ok {
-			return fmt.Sprintf("%v", value)
+			return fmt.Sprintf("%v", value), nil
 		}
 	case bool:
 		if value, ok := src.(bool); ok {
-			return strconv.FormatBool(value)
+			return strconv.FormatBool(value), nil
 		}
 	case string:
 		if value, ok := src.(string); ok {
-			return strconv.Quote(string(value))
+			return value, nil
 		}
 	case []byte:
 		// TODO handle blobs?
-		if value, ok := src.([]uint8); ok {
-			return strconv.Quote(string(value))
+		if value, ok := src.([]byte); ok {
+			return string(value), nil
 		}
 	case time.Time:
 		if value, ok := src.(time.Time); ok {
-			return strconv.Quote(value.String())
+			return value.String(), nil
 		}
 	case nil:
-		return "NULL"
+		return "NULL", nil
 	case *interface{}:
 		if src == nil {
-			return "NULL"
+			return "NULL", nil
 		}
 		return d.toSQLStringValue(*(src.(*interface{})))
 	default:
-		panic(src)
+		return "", errors.New("could not parse type")
 	}
 
-	return ""
+	return "", nil
 }
 
 func (d *textDumper) relationshipConfigToOptions(relationshipsConfig []*config.Relationship) []*reader.RelationshipOpt {

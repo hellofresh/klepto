2668aede7fc99eaa2b74d4642fd18b31f94c591f
warnar boekkooi <wbo@hellofresh.com>
2018-01-04 11:44:05 +0100

    Use copyin

--- pkg/dumper/postgres/dumper.go
+++ pkg/dumper/postgres/dumper.go
@@ -2,12 +2,14 @@ package postgres
 
 import (
 	"database/sql"
+	"sync"
 
-	sq "github.com/Masterminds/squirrel"
 	"github.com/hellofresh/klepto/pkg/config"
 	"github.com/hellofresh/klepto/pkg/database"
 	"github.com/hellofresh/klepto/pkg/dumper"
 	"github.com/hellofresh/klepto/pkg/reader"
+	"github.com/lib/pq"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -56,6 +58,8 @@ func (p *pgDumper) dumpTables(done chan<- struct{}, configTables config.Tables) 
 		return err
 	}
 
+	var wg sync.WaitGroup
+	wg.Add(len(tables))
 	for _, tbl := range tables {
 		var opts reader.ReadTableOpt
 
@@ -74,36 +78,101 @@ func (p *pgDumper) dumpTables(done chan<- struct{}, configTables config.Tables) 
 		// Create read/write chanel
 		rowChan := make(chan database.Row)
 
-		go func(tableName string) {
-			for {
-				row, more := <-rowChan
-				if !more {
-					done <- struct{}{}
-					return
-				}
-
-				insert := sq.Insert(tableName).SetMap(p.toSQLColumnMap(row)).PlaceholderFormat(sq.Dollar)
-				_, err := insert.RunWith(p.conn).Exec()
-				if err != nil {
-					log.WithError(err).WithField("table", tableName).Error("Could not insert record")
-				}
+		go func(tableName string, rowChan <-chan database.Row) {
+			if err := p.dumpTable(tableName, rowChan); err != nil {
+				log.WithError(err).WithField("table", tableName).Error("Failed to dump table")
 			}
-		}(tbl)
+
+			wg.Done()
+		}(tbl, rowChan)
 
 		p.reader.ReadTable(tbl, rowChan, opts)
+	}
+
+	go func() {
+		// Wait for all table to be dumped
+		wg.Wait()
+
+		done <- struct{}{}
+	}()
+
+	return nil
+}
+
+func (p *pgDumper) dumpTable(tableName string, rowChan <-chan database.Row) error {
+	txn, err := p.conn.Begin()
+	if err != nil {
+		return errors.Wrap(err, "failed to open transaction")
+	}
+
+	insertedRows, err := p.insertIntoTable(txn, tableName, rowChan)
+	if err != nil {
+		return errors.Wrap(err, "failed to insert rows")
+	}
+	log.WithField("table", tableName).WithField("inserted", insertedRows).Debug("inserted rows")
+
+	if err := txn.Commit(); err != nil {
+		return errors.Wrap(err, "failed to commit transaction")
 	}
 
 	return nil
 }
 
-func (p *pgDumper) toSQLColumnMap(row database.Row) map[string]interface{} {
-	sqlColumnMap := make(map[string]interface{})
-
-	for column, value := range row {
-		sqlColumnMap[column] = value
+func (p *pgDumper) insertIntoTable(txn *sql.Tx, tableName string, rowChan <-chan database.Row) (int64, error) {
+	columns, err := p.reader.GetColumns(tableName)
+	if err != nil {
+		return 0, err
 	}
 
-	return sqlColumnMap
+	logger := log.WithFields(log.Fields{
+		"table":   tableName,
+		"columns": columns,
+	})
+	logger.Debug("Preparing copy in")
+
+	stmt, err := txn.Prepare(pq.CopyIn(tableName, columns...))
+	if err != nil {
+		return 0, errors.Wrap(err, "failed to prepare copy in")
+	}
+
+	var inserted int64
+	for {
+		row, more := <-rowChan
+		if !more {
+			break
+		}
+
+		// Put the data in the correct order
+		rowValues := make([]interface{}, len(columns))
+		for i, col := range columns {
+			val := row[col]
+			switch val.(type) {
+			case []byte:
+				val = string(val.([]byte))
+			}
+
+			rowValues[i] = val
+		}
+
+		// Insert
+		_, err := stmt.Exec(rowValues...)
+		if err != nil {
+			return 0, errors.Wrap(err, "failed to copy in row")
+		}
+
+		inserted++
+	}
+
+	logger.Debug("Executing copy in")
+	if _, err := stmt.Exec(); err != nil {
+		return 0, errors.Wrap(err, "failed to exec copy in")
+	}
+
+	if err = stmt.Close(); err != nil {
+		return 0, errors.Wrap(err, "failed to close copy in statement")
+	}
+
+	return inserted, nil
 }
 
 func (p *pgDumper) relationshipConfigToOptions(relationshipsConfig []*config.Relationship) []*reader.RelationshipOpt {

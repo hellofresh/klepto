2b8f7f9d13ed61ebfaff0aa45453bf3d1e568e4e
Italo Lelis de Vietro <italolelis@gmail.com>
2018-01-31 10:56:44 +0100

    Using the new type

--- pkg/reader/reader.go
+++ pkg/reader/reader.go
@@ -22,7 +22,7 @@ type (
 		// FormatColumn returns a escaped table.column string
 		FormatColumn(tableName string, columnName string) string
 		// ReadTable returns a channel with all database rows
-		ReadTable(string, chan<- database.Row, ReadTableOpt) error
+		ReadTable(string, chan<- database.Table, ReadTableOpt) error
 		// Close closes the reader resources and releases them.
 		Close() error
 	}

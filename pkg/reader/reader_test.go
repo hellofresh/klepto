0bbf6e62e23cb806c250403251ac8966930adb93
Lucas Sant' Anna <76971778+lucass4@users.noreply.github.com>
2023-01-27 00:40:27 -0500

    fix: Remove all quotes in postgres table name

--- pkg/reader/reader_test.go
+++ pkg/reader/reader_test.go
@@ -1,7 +1,6 @@
 package reader
 
 import (
-	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -28,12 +27,6 @@ func TestNewReadTableOpt(t *testing.T) {
 				ForeignKey:      "r2-fk",
 				ReferencedTable: "r2-reference-table",
 				ReferencedKey:   "r2-reference-key",
-			},
-			{
-				Table:           strconv.Quote("TableName"),
-				ForeignKey:      "r1-fk",
-				ReferencedTable: "r1-reference-table",
-				ReferencedKey:   "r1-reference-key",
 			},
 		},
 	}
